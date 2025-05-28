f3d2c3d5b80d769b79722c04e355d2983363eddd
Ahmad Raisul Siddiq <110159905+Siddiq-Ahrais@users.noreply.github.com>
2025-05-28 18:47:03 +0700

    Initial Commit.

---
+++ TextAdvantureGame/src/def/monster.java
@@ -0,0 +1,64 @@
+//package def;
+//
+//import java.util.Scanner;
+//
+//public class monster {
+//	int playerHP;
+//	String playerWeapon;
+//	Scanner sc = new Scanner(System.in);
+//	String playerName;
+//	
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	
+//public void playerSetup() {
+//		
+//		playerHP = 15;
+//		
+//		
+//		
+//		
+//		System.out.println("Your Player HP : " + playerHP);
+//		System.out.println("Your Weapon : " + playerWeapon);
+//				
+//		
+//		
+//		System.out.println("Please enter your Name");		
+//		playerName = sc.nextLine();
+//		
+//		System.out.println("Hello " + playerName + ", let's start your Advanture!" );
+//		
+//	}
+//	
+//	
+//	
+//	
+//	
+//		public static void mon1(){
+//		String Goblin;
+//		Goblin = "1";
+//		System.out.println("Goblin : ");
+//		int Damage;
+//		int HP;
+//		HP = 15;
+//		Damage = 0 ;
+//		Damage = new java.util.Random().nextInt(5);
+//		if(HP >0) {
+//			System.out.println("Goblin attack you with his Knife!\n");
+//			playerHP = playerHP - Damage;
+//		}
+//			
+//			
+//			
+//			}
+//		
+//	
+//}
+//
+//	
+//	
+//	
+//	
